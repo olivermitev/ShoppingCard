51d4b36ff6e3991e78ad8f0ff82f074da3450574
olivermitev <mitev_oliver@yahoo.com>
2019-12-16 00:17:18 +0100

    front shop and cart shop done for now

--- ShoppingCard/App_Start/RouteConfig.cs
+++ ShoppingCard/App_Start/RouteConfig.cs
@@ -13,11 +13,20 @@ namespace ShoppingCard
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Cart", "Cart/{action}/{id}", new { controller = "Cart", action = "Index", id = UrlParameter.Optional }, new[] { "ShoppingCard.Controllers" });
+
+            routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "Shop", action = "Index", name = UrlParameter.Optional }, new[] { "ShoppingCard.Controllers" });
+
+            routes.MapRoute("SidebarPartial", "Pages/SidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "ShoppingCard.Controllers" });
+            routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] { "ShoppingCard.Controllers" });
+            routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "ShoppingCard.Controllers" });
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "ShoppingCard.Controllers" });
+
+            //routes.MapRoute(
+            //name: "Default",
+            //url: "{controller}/{action}/{id}",
+            //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
---
+++ ShoppingCard/Controllers/PagesController.cs
@@ -0,0 +1,92 @@
+﻿using ShoppingCard.Models.Data;
+using ShoppingCard.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCard.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Index/{page}
+        public ActionResult Index(string page = "")
+        {
+            // Get/set page slug
+            if (page == "")
+                page = "home";
+            // Declare model and DTO
+            PageVM model;
+            PageDTO dto;
+
+            // Get page DTO
+            using (Db db = new Db())
+            {
+                if(! db.Pages.Any(x => x.Slug.Equals(page)))
+                {
+                    return RedirectToAction("Index", new { page = "" });
+                }
+            }
+
+            
+            using (Db db = new Db())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+
+            // Set page title
+            ViewBag.PageTitle = dto.Title;
+
+            // Check for sidebar
+            if (dto.HasSidebar == true)
+            {
+                ViewBag.Sidebar = "Yes";
+            }
+            else
+            {
+                ViewBag.Sidebar = "No";
+            }
+
+            // Init model
+            model = new PageVM(dto);
+
+            // Return view with model
+            return View(model);
+        }
+
+        public ActionResult PagesMenuPartial()
+        {
+            // Declare a list od PageVM
+            List<PageVM> pageVMList;
+
+            // Get all pages except home
+            using (Db db = new Db())
+            {
+                pageVMList = db.Pages.ToArray().OrderBy(x => x.Sorting).Where(x => x.Slug != "home").Select(x => new PageVM(x)).ToList();
+            }
+
+            // Return partial view with list
+            return PartialView(pageVMList);
+        }
+
+        public ActionResult SidebarPartial()
+        {
+            // Declare model
+            SidebarVM model;
+
+            // Init model
+            using (Db db = new Db())
+            {
+                SidebarDTO dto = db.Sidebar.Find(1);
+
+                    model = new SidebarVM(dto);
+            }
+
+            // Return partial view with model
+
+            return PartialView(model);
+        }
+
+    }
+}
